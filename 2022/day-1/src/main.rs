c6be2769dde9b6e2f91aa3a449f91d0dc6e35c4c
Patrik Svensson <patrik@patriksvensson.se>
2022-12-04 22:01:04 +0100

    Day 1: Add solution for part 2

--- 2022/day-1/src/main.rs
+++ 2022/day-1/src/main.rs
@@ -1,19 +1,19 @@
 fn main() {
     let input = include_str!("../input.txt");
+    let mut results = Vec::new();
 
     let mut sum = 0;
-    let mut highest = 0;
-
     for line in input.lines() {
         if line == "" {
-            if sum > highest {
-                highest = sum;
-            }
+            results.push(sum);
             sum = 0;
         } else {
             sum += line.parse::<i32>().unwrap();
         }
     }
 
-    println!("Result = {}", highest);
+    results.sort();
+
+    println!("First = {}", results.iter().rev().take(1).sum::<i32>());
+    println!("Top 3 = {}", results.iter().rev().take(3).sum::<i32>());
 }
